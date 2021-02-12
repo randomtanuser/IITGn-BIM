cc33b3ed08a8b749aed1de43cb9192e6a3ec378d
randomtanuser <78749650+randomtanuser@users.noreply.github.com>
2021-02-12 11:48:00 +0530

    Image processing python codes

---
+++ Classwork/Image_processing.py
@@ -0,0 +1,19 @@
+#Image processing - class-1
+##-- For comments. 
+
+#Import blocks
+import matplotlib.pyplot as plt
+from scipy import misc, ndimage
+
+face = misc.face() #create the face array
+
+#Use different gaussian kernels for smoothing
+blurred_face = ndimage.gaussian_filter(face, sigma=3)
+very_blurred = ndimage.gaussian_filter(face, sigma=15)
+
+#draw the image
+
+plt.imshow(very_blurred)
+
+#show the image
+plt.show()
